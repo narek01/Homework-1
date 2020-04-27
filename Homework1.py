dc390821ad94134c8e2e7a36b5dea249ba6560c3
narek01 <narek030601@yandex.ru>
2020-04-28 01:12:07 +0300

    Написаны ввод данных, проверка ввода и зачатки самих функций bwt

--- Homework1.py
+++ Homework1.py
@@ -1,7 +1,28 @@
 #import numpy as np
-import cupy as cp
-#import matplotlib as mlt
+#import cupy as cp
+import argparse
+import sys
 
-with cp.cuda.Device(0):
-    x = cp.array([1, 2, 3, 4, 5])
-print(x)
+def direct_bwt(str):
+    return 0
+def inverse_bwt(index, str):
+    return 0
+
+#Получаем аргументы из командной строки и добавляем немножко описания.
+parser = argparse.ArgumentParser(
+        prog='Burrows-Wheeler transform',
+        description='Use index value "-1" to do direct transform',
+        epilog='Thanks for using this program!'
+        )
+parser.add_argument('string')
+parser.add_argument('index', type=int)
+args = parser.parse_args()
+
+#Проверяем входные данные и выводим ответ, если всё ок.
+if args.index < -1:
+    print('Please, use correct index value ')
+    sys.exit()
+elif args.index == -1:
+    print(direct_bwt(args.string))
+else:
+    print(inverse_bwt(args.index, args.string))
